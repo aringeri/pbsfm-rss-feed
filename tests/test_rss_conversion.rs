bf1be26ccb5f15b973c1c815c42155836b9e7ad9
Alex Ringeri <ac.ringeri@gmail.com>
2025-09-03 08:32:49 +1000

    Move rss conversion test

---
+++ tests/test_rss_conversion.rs
@@ -0,0 +1,21 @@
+use chrono::NaiveDate;
+use rss_gen::{macro_generate_rss, macro_write_element};
+use quick_xml::writer::Writer;
+use std::io::{Error};
+use std::fs::File;
+use std::io::BufWriter;
+
+mod mock_airnet;
+
+#[test]
+fn test_convert_to_rss() -> Result<(), Box<dyn std::error::Error>> {
+    let program = mock_airnet::expected::single_program();
+    let episodes = mock_airnet::expected::episodes();
+
+    let _rss_feed = pbsfm_rss_feed::convert_to_rss(program, episodes)?;
+    // let writer = Writer::new(BufWriter::new(File::create("output.xml")?));
+    // let result: Result<Writer<BufWriter<File>>, Error> = macro_generate_rss!(writer, rss_feed);
+    // assert!(result.is_ok());
+    // assert_eq!(rss_feed.title, program.name);
+    Ok(())
+}
